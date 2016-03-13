64d4179f48b3ee9e7a541902d4f1e7ed990190e9
Jason Pan <JasonRichardPan@gmail.com>
2016-03-12 22:35:38 -0800

    project setup

---
+++ _configs.py
@@ -0,0 +1,14 @@
+import os
+
+# grab the folder where the script lives
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DATABASE = 'flasktasker.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'A967382E524649E2DD9DD227D4582'
+
+# define the full path for the database
+DATABASE_PATH = os.path.join(basedir, DATABASE)
+
