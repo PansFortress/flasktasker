a2282467d0fdd2bfda598caba428dcf899148a3e
Jason Pan <JasonRichardPan@gmail.com>
2016-03-27 17:27:40 -0400

    Additional Imports from Wtforms

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import Form
-from wtforms import StringField, DateField, IntegerField, SelectField
-from wtforms.validators import DataRequired
+from wtforms import StringField, DateField, IntegerField, SelectField, PasswordField
+from wtforms.validators import DataRequired, Length, EqualTo
 
 
 class AddTaskForm(Form):
@@ -26,3 +26,4 @@ class RegistrationForm(Form):
 class LoginForm(Form):
     name = StringField('Username', validators = [DataRequired()])
     password = PasswordField('Password', validators = [DataRequired()])
+
